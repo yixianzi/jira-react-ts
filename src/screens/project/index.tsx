d1e1180c64f206d4d661d3591c9520d95c7f560e
guoxianyi <zw-guoxy@chinaunicom.cn>
2021-10-20 15:58:09 +0800

    feat: 看板和路由

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router";
+import { Link } from "react-router-dom";
+import { EpicScreen } from "screens/epic";
+import { KanbanScreen } from "screens/Kanban";
 
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>;
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={"kanban"}>看板</Link>
+      <Link to={"epic"}>任务组</Link>
+      <Routes>
+        <Route path={"/kanban"} element={<KanbanScreen />}></Route>
+        <Route path={"/epic"} element={<EpicScreen />}></Route>
+        <Navigate to={window.location.pathname + "/kanban"}></Navigate>
+      </Routes>
+    </div>
+  );
 };
