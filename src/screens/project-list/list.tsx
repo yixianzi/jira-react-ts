560d27c340918d5884067a0baa1d428975294ad5
guoxianyi <zw-guoxy@chinaunicom.cn>
2021-10-20 15:25:18 +0800

    feat: 添加路由：projects和project

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -2,6 +2,7 @@ import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
 import React from "react";
 import { User } from "./search-panel";
+import { Link } from "react-router-dom";
 
 export interface Project {
   id: string;
@@ -24,8 +25,10 @@ export const List = ({ users, ...props }: ListProps) => {
       columns={[
         {
           title: "名称",
-          dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
         },
         {
           title: "部门",
