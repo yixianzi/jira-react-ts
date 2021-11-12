a91121085b7898d57d0358c13ae8ac854fe2a0bd
guoxianyi <zw-guoxy@chinaunicom.cn>
2021-11-12 15:19:03 +0800

    chore: fix

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 import { Form, Input } from 'antd'
 import { LongButton } from 'unauthenticated-app'
 import { useAsync } from 'utils/use-async'
-import { useDispatch } from 'react-redux'
 
 export const LoginScreen = ({ onError }: { onError: (error: Error) => void }) => {
   const { login } = useAuth()
   const { run, isLoading } = useAsync(undefined, { throwOnError: true })
-  const dispatch = useDispatch()
 
   const handleSubmit = async (values: { username: string; password: string }) => {
     try {
